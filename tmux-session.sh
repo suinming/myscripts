7a2efef4fef12197701bfd8e6ebdefeaa4822f14
suinming <0223314338aa@gmail.com>
2025-09-14 20:24:16 +0800

    feat: add tmux-session script

---
+++ tmux-session.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+DIRS=(
+    "$HOME/repo"
+    "$HOME"
+)
+
+if [[ $# -eq 1 ]]; then
+    selected=$1
+    [[ ! -d "$selected" ]] && { echo "Error: $selected is not a directory"; exit 1; }
+else
+    selected=$(find "${DIRS[@]}" -maxdepth 1 -type d \
+        | sed "s|^$HOME/||" \
+        | fzf --margin 10% --color="bw")
+    [[ $selected ]] && selected="$HOME/$selected"
+fi
+
+[[ ! $selected ]] && exit 0
+
+selected_name=$(basename "$selected" | tr . _)
+
+if ! tmux has-session -t "$selected_name" 2>/dev/null; then
+    tmux new-session -ds "$selected_name" -c "$selected"
+    tmux select-window -t "$selected_name:1"
+fi
+
+tmux switch-client -t "$selected_name"
